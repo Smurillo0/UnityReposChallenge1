9377344b3ae4ff706f426fd5effc58b65afd95c0
Smurillo0 <smurillo.0103@gmail.com>
2023-09-11 09:44:47 -0500

    Added Challenge1

---
+++ Challenge1/Assets/Challenge 1/Scripts/YouLose.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouLose : MonoBehaviour
+{
+    public GameObject player;
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y < 80 && player.transform.position.y > -51)
+        {
+            ScoreManager.gameOver = true;
+
+        }
+    }
+}
