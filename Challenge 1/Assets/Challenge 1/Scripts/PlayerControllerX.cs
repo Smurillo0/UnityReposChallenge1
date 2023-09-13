149de45d939b228543d8c673dc354f7ee3e9f6e8
Smurillo0 <smurillo.0103@gmail.com>
2023-09-12 21:36:10 -0500

    added Challenge1

---
+++ Challenge 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,39 @@
+﻿/*
+    * Sarahi Murillo
+    * PlayControllerX.cs
+    * Challenge 1 
+    * Added the verticalInput to the rotate function and changed .back to .forward in the translate fucntion to make it go forward. 
+ 
+
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
---
+++ Challenge 1/Assets/Challenge 1/Scripts/TriggerZoneAddOne.cs
@@ -0,0 +1,27 @@
+﻿
+/*
+    * Sarahi Murillo
+    * TriggerZoneAddOne.cs
+    * Challenge 1 
+    * this script allows us to check for collisions with trigger zones placed on the plane and inccremnet by 1 each time a target is hit. 
+    
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneAddOne : MonoBehaviour
+{
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            ScoreManager.score++;
+        }
+
+    }
+}
